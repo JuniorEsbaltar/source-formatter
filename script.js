526c03abd5af75eef3c0b0bab99773dce74276ab
Augusto <junior.esbaltar@gmail.com>
2022-08-07 00:09:23 -0300

    refactor(style): use display grid and add copy action

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const formatSource = (source) => {
-  const queryObj = JSON.parse(source);
+  const queryObj = isObject(source);
 
   const query = document.getElementById("query");
   const variables = document.getElementById("variables");
-
+  
   query.innerHTML = queryObj.query || '';
   variables.innerHTML = JSON.stringify(queryObj.variables, undefined, 2) || '';
+}
+
+const isObject = (obj) => {
+  try {
+    return JSON.parse(obj);
+  } catch { 
+    return {}
+  }
 }
 
 const textarea = document.getElementById("unformatedSource");
@@ -13,3 +21,8 @@ const textarea = document.getElementById("unformatedSource");
 textarea.addEventListener('input', function handleChange(event) {
   formatSource(event.target.value);
 });
+
+const copyAction = (inputId) => {
+  const input = document.getElementById(inputId);
+  navigator.clipboard.writeText(input.value);
+}
